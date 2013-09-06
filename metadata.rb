08de9fb611c76d46b2df0327e869686941f2ee84
Ian Mulvany <ian@mulvany.net>
2013-09-06 16:06:16 +0100

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,4 @@
+maintainer       "Ian Mulvany"
+maintainer_email "i.mulvany@elifesciecnes.org"
+license          "Apache 2.0"
+description      "installs scripts for configuring a drupal 7 instance"
