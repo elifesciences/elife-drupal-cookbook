c0bd0d9411559c9d32701b62bbf92de3f50f1e80
Ian Mulvany <ian@mulvany.net>
2013-09-16 15:06:36 +0100

    add directive to ignore table if it already exists, create elife table

--- recipes/example.rb
+++ recipes/example.rb
@@ -4,20 +4,29 @@ include_recipe "mysql"
 include_recipe "drush"
 include_recipe "drush_make"
 
-# # Add an admin user to mysql
-# execute "add-admin-user" do
-#   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
-#       "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'localhost' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;" +
-#       "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'%' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;\" " +
-#       "mysql"
-#   action :run
-# end
+# Add an admin user to mysql
+execute "add-admin-user" do
+  command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+      "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'localhost' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;" +
+      "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'%' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;\" " +
+      "mysql"
+  action :run
+end
 
-# # TODO: Break this out into a vagrant only cookbook? (name: "drupal-vagrant")
-# # create a drupal db
-# execute "add-drupal-db" do
+# TODO: Break this out into a vagrant only cookbook? (name: "drupal-vagrant")
+# create a drupal db
+execute "add-drupal-db" do
+  command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+       "CREATE DATABASE IF NOT EXISTS drupal;\""
+       "CREATE DATABASE IF NOT EXISTS elife;\""
+  action :run
+  ignore_failure true
+end
+
+# create elife user and grant priveldeges
+# execute "create-elife-user" do
 #   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
-#       "CREATE DATABASE drupal;\""
+#       "GRANT ALL PRIVILEGES ON 'elife'.* TO 'elife'@'localhost' IDENTIFIED BY '#{node[:mysql][:elife_user_password]}' WITH GRANT OPTION;\""
 #   action :run
 #   ignore_failure true
 # end
