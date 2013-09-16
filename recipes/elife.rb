59d78813e057ef8ea3d768ce1e987d5eb6d8d3b2
Ian Mulvany <ian@mulvany.net>
2013-09-16 15:06:01 +0100

    prototype script for auto configuring elife

---
+++ recipes/elife.rb
@@ -0,0 +1,73 @@
+# Deploy an example Drupal site.
+# TODO Move this to a definition with parameters.
+include_recipe "mysql"
+include_recipe "drush"
+include_recipe "drush_make"
+
+# # Add an admin user to mysql
+# execute "add-admin-user" do
+#   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+#       "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'localhost' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;" +
+#       "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'%' IDENTIFIED BY 'myadmin' WITH GRANT OPTION;\" " +
+#       "mysql"
+#   action :run
+# end
+
+# # TODO: Break this out into a vagrant only cookbook? (name: "drupal-vagrant")
+# # TODO: prevent this from running if we have already got these databases in place 
+# # create a drupal db and eLife db 
+# execute "add-drupal-db" do
+#   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+#       "CREATE DATABASE IF NOT EXISTS drupal;\""
+#       "CREATE DATABASE IF NOT EXISTS elife;\""
+#   action :run
+#   ignore_failure true
+# end
+
+# # create elife user and grant priveldeges
+# execute "create-elife-user" do
+#   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+#       "CREATE USER 'elife'@'localhost' IDENTIFIED BY '#{node[:mysql][:elife_user_password]}';"
+#       "GRANT ALL PRIVILEGES ON elife.* TO 'elife'@'localhost' IDENTIFIED BY 'elife' WITH GRANT OPTION;" +
+#   action :run
+#   ignore_failure true
+# end
+
+# # load elife database dump 
+# execute "load-elife-db" do
+#   command "/usr/bin/mysql -u elife -p#{node[:mysql][:elife_user_password]} -e \"" +
+#       elife < "drupal_prod_jnl_elife.latest.sql;\""
+#   action :run
+#   ignore_failure true
+# end
+
+# drush make a default drupal site example
+bash "install-default-drupal-makefile" do
+  code <<-EOH
+  mkdir -p /vagrant/public/drupal.vbox.local
+  EOH
+  not_if { File.exists?("/vagrant/public/drupal.vbox.local/example.make") }
+end
+
+# Copy make file to site.
+# TODO Fetch this file online?
+# TODO Does this overwrite?
+cookbook_file "/vagrant/public/drupal.vbox.local/example.make" do
+  source "example.make"
+  notifies :restart, resources("service[apache2]"), :delayed
+end
+
+# drush make a default drupal site example
+bash "install-default-drupal-site" do
+  code <<-EOH
+  cd /vagrant/public/drupal.vbox.local
+  drush make example.make www
+  EOH
+  not_if { File.exists?("/vagrant/public/drupal.vbox.local/www/index.php") }
+end
+
+# we are not using varnish for now, so don't set this
+# cookbook_file "/vagrant/public/drupal.vbox.local/www/sites/default/settings.php" do
+#   source "settings.php"
+#   notifies :restart, resources("service[varnish]"), :delayed
+# end
