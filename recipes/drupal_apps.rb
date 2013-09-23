1fd1025cfa7c4b5273b407f142a58487e0d2f6b7
David Moulton <d.moulton@elifesciences.org>
2013-09-23 12:58:07 +0100

    Modified docroot to try to get elife site running locally

--- recipes/drupal_apps.rb
+++ recipes/drupal_apps.rb
@@ -10,7 +10,9 @@ include_recipe "apache2"
       template "sites.conf.erb"
       server_name site
       server_aliases [site]
-      docroot "#{node['www_root']}/#{site}/www"
+# modified docroot: experiment to try to get elife site running locally
+#      docroot "#{node['www_root']}/#{site}/www"
+      docroot "#{node['www_root']}/#{site}/www/drupal-webroot"
     end
   end
 #end
