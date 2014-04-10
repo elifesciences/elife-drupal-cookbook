6a1ddfc95965a4e5456ec0141db34ff9bf94b015
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2014-04-10 11:30:03 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,15 +2,17 @@ maintainer	"Ian Mulvany"
 maintainer_email "i.mulvany@elifesciecnes.org"
 license		"Apache 2.0"
 description	"installs scripts for configuring a Drupal 7.x instance"
-version		"0.2.1"
+version		"0.3.0"
 
 %w{ debian ubuntu fedora amazon }.each do |os|
   supports os
 end
 
+depends	"apt"
 depends	"php"
 depends	"apache2"
 depends	"mysql"
 depends	"phpmyadmin"
 depends	"memcached"
 depends	"varnish"
+depends	"elife-drush-cookbook"
